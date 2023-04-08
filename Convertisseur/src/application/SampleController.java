0ee8b15776c34fbcbb0d8f9539f823e9db4ab0ff
tkmyt <tkmyt@192.168.2.54>
2023-04-08 10:09:45 -0400

    8 avril 2023

--- Convertisseur/src/application/SampleController.java
+++ Convertisseur/src/application/SampleController.java
@@ -1,3 +1,8 @@
+/*
+*Author:Tristan Kako
+*Date:5 avril, 2023
+*Description:Application qui permet de convertir des unités divers, y compris des conversions entre le système impérial et le système metrique.
+*/
 package application;
 
 import java.net.URL;
@@ -16,9 +21,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.KeyEvent;
 
-public class SampleController implements Initializable
+public class SampleController implements Initializable // Pour initializer les combo box
 {
-
+	// Déclaration des fxId pour tous les éléments utilisés dans le fichier .fxml
     @FXML
     private ComboBox<String> ScboBox1;
 
@@ -81,27 +86,27 @@ public class SampleController implements Initializable
 
     
 
-    @FXML
+    @FXML // Listes avec les unités de stockage et leurs conversions
     private ObservableList<String> listInformations=FXCollections.observableArrayList("Bit","Octet","Kilo-octet","Mégaoctet","Gigaoctet");
     private double []informations= {0.125,1.0,1000.0,1000000.0,1000000000.0};
     
-    @FXML
+    @FXML // Listes avec les unités de volume et leurs conversions
     private ObservableList<String> listVolumes=FXCollections.observableArrayList("Millilitre","Litre","Mètre cube","Pouce cube","Pied cube");
     private double []volumes= {1.0,1000.0,1000000.0,16.387,28320};
     
-    @FXML
+    @FXML // Listes avec les unités de fréquence et leurs conversions
     private ObservableList<String> listFréquences=FXCollections.observableArrayList("Hertz","Kilohertz","Mégahertz","Gigahertz");
     private double []fréquences= {1.0,1000.0,1000000.0,1000000000.0};
     
-    @FXML
+    @FXML // Listes avec les unités d'énergie et leurs conversions
     private ObservableList<String> listÉnergies=FXCollections.observableArrayList("Joule","Kilojoule","Kilowattheure","Gramme de calories","Unité thermique britannique");
     private double []énergies= {1.0,1000.0,3600000.0,4.184,1055.0};
     
-    @FXML
+    @FXML // Listes avec les unités de masse et leurs conversions
     private ObservableList<String> listMasses=FXCollections.observableArrayList("Milligramme","Gramme","Kilogramme","Livre","Tonne");
     private double []masses= {0.001,1.0,1000.0,453.6,1000000.0};
     
-	@Override
+	@Override // Méthode qui met les options d'unité dans les combo box et selectionne la première unité de chacun
 	public void initialize(URL location, ResourceBundle resources)
 	{
 		ScboBox1.setItems(listInformations);
@@ -131,21 +136,21 @@ public class SampleController implements Initializable
 		
 	}
 	
-	@FXML
+	@FXML // Méthode qui prend en paramètres un combo box et une liste de double
 	private double setConv(ComboBox a, double b[])
 	{
 		int item=a.getSelectionModel().getSelectedIndex();
 		double val=b[item];
-		return val;
+		return val; // Retourne la valeur de la liste à un indexe égal à celui de l'unité selectionné dans le combo box
 	}
 	
-	@FXML
+	@FXML // Méthode qui prend en paramètres deux combo box, deux text fields et un liste de double, et permet la conversion d'unités
 	private void convert(ComboBox a, ComboBox b, TextField c, TextField d, double e[])
 	{
 		double from=setConv(a,e);
 		double depart=0;
 		
-		if (c.getText().equals(""))
+		if (c.getText().equals("")) // S'il n'y a pas de texte dans le text field, on utilise 0 comme valeur de départ
 		{
 			depart=0;
 		}
@@ -154,18 +159,18 @@ public class SampleController implements Initializable
 			depart=Double.parseDouble(c.getText());
 		}
 		double to=setConv(b,e);
-		double dest=(from/to)*depart;
-		d.setText(String.valueOf(dest));
+		double dest=(from/to)*depart; // Fait la conversion entre l'unité du combo box a et l'unité du combo box b
+		d.setText(String.valueOf(dest)); // Met la valeur obtenue dans le text field qui n'a pas encore été utilisé
 	}
 	
 	@FXML
-	private void verifNum(KeyEvent e)
+	private void verifNum(KeyEvent e) // Méthode qui permet de vérifier si l'entrée dans un text field est numérique
 	{
 		TextField txt=(TextField)e.getSource();
 
 		txt.textProperty().addListener((observable,oldValue,newValue)->
 		{
-			if(!newValue.matches("^-?[0-9](\\.[0-9]+)?$"))
+			if(!newValue.matches("^-?[0-9](\\.[0-9]+)?$")) // Vérifie si le caractère est un chiffre entre 0 et 9
 			{
 				txt.setText(newValue.replaceAll("[^\\d*\\.\\-]",""));
 			}
@@ -173,7 +178,7 @@ public class SampleController implements Initializable
 	});
 	}
 	
-	public void quitter()
+	public void quitter() // Méthode qui vérifie si l'utilisateur veut réellement quitter
     {
     	Alert alert=new Alert(AlertType.CONFIRMATION);
 		alert.setHeaderText("Confirmation");
@@ -181,64 +186,64 @@ public class SampleController implements Initializable
 		alert.setContentText("Voudrais-tu vraiment quitter?");
 		Optional<ButtonType> result=alert.showAndWait();
 		if(result.get()==ButtonType.OK)
-			System.exit(0);
+			System.exit(0); // Ferme l'application
     }
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à gauche pour le stockage
 	private void ConvertS1()
 	{
 		convert(ScboBox1,ScboBox2,StxtField1,StxtField2,informations);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à droite pour le stockage
 	private void ConvertS2()
 	{
 		convert(ScboBox2,ScboBox1,StxtField2,StxtField1,informations);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à gauche pour le volume
 	private void ConvertV1()
 	{
 		convert(VcboBox1,VcboBox2,VtxtField1,VtxtField2,volumes);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à droite pour le volume
 	private void ConvertV2()
 	{
 		convert(VcboBox2,VcboBox1,VtxtField2,VtxtField1,volumes);
 	}
 
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à gauche pour la fréquence
 	private void ConvertF1()
 	{
 		convert(FcboBox1,FcboBox2,FtxtField1,FtxtField2,fréquences);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à droite pour la fréquence
 	private void ConvertF2()
 	{
 		convert(FcboBox2,FcboBox1,FtxtField2,FtxtField1,fréquences);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à gauche pour l'énergie
 	private void ConvertE1()
 	{
 		convert(EcboBox1,EcboBox2,EtxtField1,EtxtField2,énergies);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à droite pour l'énergie
 	private void ConvertE2()
 	{
 		convert(EcboBox2,EcboBox1,EtxtField2,EtxtField1,énergies);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à gauche pour la masse
 	private void ConvertM1()
 	{
 		convert(McboBox1,McboBox2,MtxtField1,MtxtField2,masses);
 	}
 	
-	@FXML
+	@FXML // Conversion pour le text field et le combo box à droite pour la masse
 	private void ConvertM2()
 	{
 		convert(McboBox2,McboBox1,MtxtField2,MtxtField1,masses);
